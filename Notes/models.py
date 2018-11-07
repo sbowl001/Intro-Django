466f6a1bb7e0dbdae2b81645ba06569a19296c9a
Stephanie Bowles <sbowl001@fiu.edu>
2018-11-08 00:15:56 +0800

    day 1

---
+++ Notes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from uuid import uuid4
+# Create your models here.
+
+class Note (models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
+    title = models.CharField( max_length = 30)
+    body = models.TextField( blank = True )
+    author = models.CharField( max_length = 25)
+    date = models.DateTimeField(auto_now = True)
+    completed = models.BooleanField(default = False)
